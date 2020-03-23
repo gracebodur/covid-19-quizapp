c307bfaa111f9b401b07b6ff7f50656a529de117
Grace <mgracebodur@gmail.com>
2020-03-23 03:26:55 -0400

    Added questions, and disclaimer

---
+++ store.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const QUIZ = {
+    questions: [
+        {
+            text: "What Is Coronavirus?",
+            answers: [
+                {
+                    text:"COVID-19 is the disease caused by the change of weather",
+                    isCorrect: false,
+                },
+                {
+                    text: "The coronavirus is comparable to seasonal influenza",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 is the disease caused by the new coronavirus that emerged in China in December 2019. COVID-19 symptoms include cough, fever and shortness of breath. COVID-19 can be severe, and some cases have caused death.",
+                    isCorrect: true,
+                },
+                {
+                    text: "The virus is thought to spread mainly between people who are not in contact with one another",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "How is COVID-19 spread?",
+            answers: [
+                {
+                    text:"COVID-19 virus can be transmitted in areas with hot and humid climates",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 virus can be transmitted in areas with cold and dry climatess",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 virus can be transmitted in ALL AREAS. COVID-19 can be passed from person to person through droplets from coughs and sneezes.",
+                    isCorrect: true,
+                },
+                {
+                    text: "COVID-19 virus is not contagious",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "How is COVID-19 diagnosed?",
+            answers: [
+                {
+                    text:"Diagnosis may be difficult with only a physical exam because mild cases of COVID-19 may appear similar to the flu or a bad cold. A laboratory test can confirm the diagnosis. ",
+                    isCorrect: true,
+                },
+                {
+                    text: "COVID-19 can be confirm by an on-the-counter test kit",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 tests can take an hour to get the results",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 cases can not be diagnosed",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "What are symptoms of COVID-19?",
+            answers: [
+                {
+                    text:"COVID-19 symptoms include: snoring, sweating and frequent urination.",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 symptoms include: strong appetite and normal body temperature",
+                    isCorrect: false,
+                },
+                {
+                    text: "COVID-19 symptoms may appear 2-14 days after exposure include: Cough, fever and shortness of breath.",
+                    isCorrect: true,
+                },
+                {
+                    text: "COVID-19 symptoms include: increased thirst, extreme hunger and itchy skin",
+                    isCorrect: false,
+                },
+            ],
+        },
+        {
+            text: "How is COVID-19 treated?",
+            answers: [
+                {
+                    text:"Taking a hot bath will help treat COVID-19.",
+                    isCorrect: false,
+                },
+                {
+                    text: "Taking antibiotics can treat COVID-19",
+                    isCorrect: false,
+                },
+                {
+                    text: "Eating garlic help prevent infection with the new coronavirus",
+                    isCorrect: false,
+                },
+                {
+                    text: "As of now, there is not a specific treatment for the virus.The best way to protect yourself against COVID-19 is by frequently cleaning your hands.",
+                    isCorrect: true,
+                },
+            ],
+        },
+
+    ],
+
+    answers: [],
+}
