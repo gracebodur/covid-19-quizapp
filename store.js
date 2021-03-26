fa57b8ce12bd65ee87572a34534821063dbfe7de
Grace <mgracebodur@gmail.com>
2021-03-26 13:09:10 -0400

    update quiz questions

--- store.js
+++ store.js
@@ -1,114 +1,182 @@
-'use strict';
+'use strict'
 
 const QUIZ = {
-    questions: [
+	questions: [
+		{
+			text: 'What is a virus?',
+			answers: [
+				{
+					text: 'A microscopic, single-celled organism',
+					isCorrect: false,
+				},
+				{
+					text:
+						'A sub-microscopic infectious agent that replicates only inside the living cells of an organism',
+					isCorrect: true,
+				},
+				{
+					text: 'A multicellular organism with chlorophyll as its primary photosynthetic pigment',
+					isCorrect: false,
+				},
+			],
+		},
+		{
+			text:
+				'What is the official name of the virus as designated by the World Health Organization (WHO)',
+			answers: [
+				{
+					text: 'Sars-CoV-2',
+					isCorrect: true,
+				},
+				{
+					text: 'COVID-19',
+					isCorrect: false,
+				},
+				{
+					text: 'Wuhan flu',
+					isCorrect: false,
+				},
+			],
+		},
+		{
+			text: 'What does the 19 in Covid-19 stand for',
+			answers: [
+				{
+					text: 'It refers to the 19 molecules that make up the virus',
+					isCorrect: false,
+				},
+				{
+					text: 'It is the 19th coronavirus identified since the WHO began naming them',
+					isCorrect: false,
+				},
+				{
+					text: 'It is the year the virus was first encountered: 2019',
+					isCorrect: true,
+				},
+			],
+		},
+		{
+			text:
+				'About what percentage of infected people recover without needing hospital treatment according to the World Health Organisation website?',
+			answers: [
+				{
+					text: '60%',
+					isCorrect: false,
+				},
+				{
+					text: '70%',
+					isCorrect: false,
+				},
+				{
+					text: '80%',
+					isCorrect: true,
+				},
+				{
+					text: '90%',
+					isCorrect: false,
+				},
+			],
+		},
+		{
+			text: 'Which of these is NOT listed by the WHO as a symptom of coronavirus?',
+			answers: [
+				{
+					text: 'Fever',
+					isCorrect: false,
+				},
+				{
+					text: 'Dry cough',
+					isCorrect: false,
+				},
+				{
+					text: 'Blurred vision',
+					isCorrect: true,
+				},
+				{
+					text: 'Nasal congestion',
+					isCorrect: false,
+				},
+			],
+		},
+		{
+			text: 'What is more effective at removing the coronavirus from your hands',
+			answers: [
+				{
+					text: 'Alcohol-based hand sanitiser',
+					isCorrect: false,
+				},
+				{
+					text: 'Soap and water',
+					isCorrect: true,
+				},
+			],
+		},
+		{
+			text: 'How big is the coronavirus?',
+			answers: [
+				{
+					text: '8 billionths of a metre in diameter',
+					isCorrect: false,
+				},
+				{
+					text: '80 billionths of a metre in diameter',
+					isCorrect: true,
+				},
+				{
+					text: '800 billionths of a metre in diameter',
+					isCorrect: false,
+				},
+			],
+		},
         {
-            text: "What Is Coronavirus?",
-            answers: [
-                {
-                    text:"COVID-19 is the disease caused by the change of weather",
+            text: 'What does the virus attach itself to when it enters the human body?',
+            answers: [ {
+                    text: 'Antigens',
                     isCorrect: false,
                 },
                 {
-                    text: "The coronavirus is comparable to seasonal influenza",
+                    text: 'Red blood cells',
                     isCorrect: false,
                 },
                 {
-                    text: "COVID-19 is the disease caused by the new coronavirus that emerged in China in December 2019. COVID-19 symptoms include cough, fever and shortness of breath. COVID-19 can be severe, and some cases have caused death.",
+                    text: 'Ace-2 receptors in the lining of the airways',
+                    isCorrect: true,
+                },
+            ],
+        },
+        {
+            text: 'The virus replicates by inserting its RNA into a human cell’s own replication machinery. It makes multiple copies of itself, and these burst out of the cell, causing the infection to spread. What does RNA stand for?',
+            answers: [ {
+                    text: 'Ribonucleic acid',
                     isCorrect: true,
                 },
                 {
-                    text: "The virus is thought to spread mainly between people who are not in contact with one another",
+                    text: 'Reductone nano articulate',
+                    isCorrect: false,
+                },
+                {
+                    text: 'Reproducing nucleic agent',
                     isCorrect: false,
                 },
             ],
         },
         {
-            text: "How is COVID-19 spread?",
-            answers: [
-                {
-                    text:"COVID-19 virus can be transmitted in areas with hot and humid climates",
-                    isCorrect: false,
-                },
-                {
-                    text: "COVID-19 virus can be transmitted in areas with cold and dry climatess",
-                    isCorrect: false,
-                },
-                {
-                    text: "COVID-19 virus can be transmitted in ALL AREAS. COVID-19 can be passed from person to person through droplets from coughs and sneezes.",
+            text: 'How long can the virus survive on plastic and stainless steel surfaces, according to studies?',
+            answers: [ {
+                    text: '72 hours or more',
                     isCorrect: true,
                 },
                 {
-                    text: "COVID-19 virus is not contagious",
+                    text: '24 to 60 hours',
+                    isCorrect: false,
+                },
+                {
+                    text: '4 to 12 hours',
                     isCorrect: false,
                 },
             ],
         },
-        {
-            text: "How is COVID-19 diagnosed?",
-            answers: [
-                {
-                    text:"Diagnosis may be difficult with only a physical exam because mild cases of COVID-19 may appear similar to the flu or a bad cold. A laboratory test can confirm the diagnosis. ",
-                    isCorrect: true,
-                },
-                {
-                    text: "COVID-19 can be confirm by an on-the-counter test kit",
-                    isCorrect: false,
-                },
-                {
-                    text: "COVID-19 tests can take an hour to get the results",
-                    isCorrect: false,
-                },
-                {
-                    text: "COVID-19 cases can not be diagnosed",
-                    isCorrect: false,
-                },
-            ],
-        },
-        {
-            text: "What are symptoms of COVID-19?",
-            answers: [
-                {
-                    text:"COVID-19 symptoms include: snoring, sweating and frequent urination.",
-                    isCorrect: false,
-                },
-                {
-                    text: "COVID-19 symptoms include: strong appetite and normal body temperature",
-                    isCorrect: false,
-                },
-                {
-                    text: "COVID-19 symptoms may appear 2-14 days after exposure include: Cough, fever and shortness of breath.",
-                    isCorrect: true,
-                },
-                {
-                    text: "COVID-19 symptoms include: increased thirst, extreme hunger and itchy skin",
-                    isCorrect: false,
-                },
-            ],
-        },
-        {
-            text: "How is COVID-19 treated?",
-            answers: [
-                {
-                    text:"Taking a hot bath will help treat COVID-19.",
-                    isCorrect: false,
-                },
-                {
-                    text: "Taking antibiotics can treat COVID-19",
-                    isCorrect: false,
-                },
-                {
-                    text: "Eating garlic help prevent infection with the new coronavirus",
-                    isCorrect: false,
-                },
-                {
-                    text: "As of now, there is not a specific treatment for the virus.The best way to protect yourself against COVID-19 is by frequently cleaning your hands.",
-                    isCorrect: true,
-                },
-            ],
-        },
+	],
 
-    ],
-
-    answers: [],
+	answers: [],
 }
